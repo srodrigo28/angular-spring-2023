3df785ddbd2be346e25e7cc3a918303c5c2a9523
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-08-26 15:03:48 -0300

    atualizado com sucesso modulo de post com mensagem

--- crud-angular/src/app/condominio/condominio-form/condominio-form.component.ts
+++ crud-angular/src/app/condominio/condominio-form/condominio-form.component.ts
@@ -3,12 +3,12 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CondominioService } from '../services/condominio.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-condominio-form',
   templateUrl: './condominio-form.component.html',
-  styleUrls: ['./condominio-form.component.scss'],
-
+  styleUrls: ['./condominio-form.component.scss']
 })
 export class CondominioFormComponent {
   form: FormGroup;
@@ -17,7 +17,8 @@ export class CondominioFormComponent {
     private formBuilder: FormBuilder,
     private router: Router,
     private service: CondominioService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private location: Location,
   ) {
     this.form = this.formBuilder.group({
       nome: [null],
@@ -29,22 +30,21 @@ export class CondominioFormComponent {
       valor_contrato: [null]
     });
   }
-
+  onCancel() {
+    this.location.back();
+  }
   onSubmit() {
     // console.log(this.form.value);
     // this.service.save(this.form.value)
-    this.service.save(this.form.value).subscribe(result => console.log(result), error => this.onError());
+    this.service.save(this.form.value)
+      .subscribe(result => this.onSuccess(), error => this.onError());
+  }
+  onSuccess() {
+    this.snackBar.open('Gravado com sucesso!', '', { duration: 1200 });
+    this.onCancel();
   }
   onError() {
-    this.snackBar.open('Erro ao gravar os dados Chamar o suprote 62-98592-1140', '', { duration: 800 });
-  }
-
-  onMembro() {
-    this.router.navigate(['membro']);
-  }
-
-  onModal() {
-    // this.router.navigate(['/']);
+    this.snackBar.open('Erro ao gravar os dados Chamar o suprote 62-98592-1140', '', { duration: 1200 });
   }
   // ngOnInit(): void{}
 
